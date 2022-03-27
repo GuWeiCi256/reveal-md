40be369c68471984f901e0592dd8967ce7677a90
GuWeiCi256 <86562116+GuWeiCi256@users.noreply.github.com>
2022-03-27 17:21:38 +0800

    Update preproc.js

--- test/preproc.js
+++ test/preproc.js
@@ -5,6 +5,7 @@ module.exports = (markdown, _options) =>
       .split('\n')
       .map((line, index) => {
         if (!/^#/.test(line) || index === 0) return line;
+        if (/^\+ /.test(line) || (/ \+ /.test(line))) line +=' <!-- .element: class="fragment"-->' ;
         const is_vertical = /#\^/.test(line);
         return (is_vertical ? '\n----\n\n' : '\n---\n\n') + line.replace('#^', '#');
       })
